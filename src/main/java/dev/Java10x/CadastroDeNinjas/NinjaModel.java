fe7ff15f97c8e58de6cffb5f4684eb8129b663ee
Guilherme Chagas <guilherme.contatosv@gmail.com>
2025-03-26 15:51:45 -0300

    Criar uma entidade no banco de dados apartir do ninjaModel

---
+++ src/main/java/dev/Java10x/CadastroDeNinjas/NinjaModel.java
@@ -0,0 +1,67 @@
+package dev.Java10x.CadastroDeNinjas;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+// JPA = Java Persistence API
+
+// ENTITY = Ela é usada para marcar uma classe como entidade persistente
+@Entity
+// TABLE = Ela define as propriedades de mapeamento entre a entidade e a tabela do banco de dados
+@Table(name = "tb_cadastro")
+public class NinjaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nome;
+    private String email;
+    private int idade;
+
+    //No args = Contrutor sem argumentos
+    public NinjaModel() {
+    }
+
+    // Al args = Construtor com argumentos
+    public NinjaModel(long id, String nome, String email, int idade) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    // Getter e Setters
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
