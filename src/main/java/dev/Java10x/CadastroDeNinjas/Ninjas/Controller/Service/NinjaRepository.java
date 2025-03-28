fa269d650ae26890a55119073a13fcb19a65af43
Guilherme Chagas <guilherme.contatosv@gmail.com>
2025-03-28 12:35:02 -0300

    buid: conecta o banco de dados H2 a minha aplicação SpringBoot

--- src/main/java/dev/Java10x/CadastroDeNinjas/Ninjas/Controller/Service/NinjaRepository.java
+++ src/main/java/dev/Java10x/CadastroDeNinjas/Ninjas/Controller/Service/NinjaRepository.java
@@ -1,2 +1,6 @@
-package dev.Java10x.CadastroDeNinjas.Ninjas.Controller.Service;public interface NinjaRepository {
+package dev.Java10x.CadastroDeNinjas.Ninjas.Controller.Service;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NinjaRepository extends JpaRepository<NinjaModel, Long> {
 }
