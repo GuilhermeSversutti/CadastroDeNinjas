b41ab50a3fdd557760696b7a2e89ef3b52f8df8f
Guilherme Chagas <guilherme.contatosv@gmail.com>
2025-03-31 11:36:44 -0300

    Chore: Inclui a versão do DTO pela entidade e mapeia manualmente no NinjaMapper os caminhos entidades x dto

--- src/main/java/dev/Java10x/CadastroDeNinjas/Ninjas/Controller/Service/NinjaDTO.java
+++ src/main/java/dev/Java10x/CadastroDeNinjas/Ninjas/Controller/Service/NinjaDTO.java
@@ -1,4 +1,23 @@
 package dev.Java10x.CadastroDeNinjas.Ninjas.Controller.Service;
 
+import dev.Java10x.CadastroDeNinjas.Missoes.MissoesModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaDTO {
+
+    //DTO -- Objeto de transferencia de dados
+    //DTO copia da entidade do banco de dados
+
+    private long id;
+    private String nome;
+    private String email;
+    private String imgUrl;
+    private int idade;
+    private String rank;
+    private MissoesModel missoes;
 }
--- src/main/java/dev/Java10x/CadastroDeNinjas/Ninjas/Controller/Service/NinjaMapper.java
+++ src/main/java/dev/Java10x/CadastroDeNinjas/Ninjas/Controller/Service/NinjaMapper.java
@@ -1,4 +1,35 @@
 package dev.Java10x.CadastroDeNinjas.Ninjas.Controller.Service;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class NinjaMapper {
+
+    //Mapper -- explicar para o java o DTO para a entidade
+
+    public NinjaModel map(NinjaDTO ninjaDTO){
+        NinjaModel ninjaModel = new NinjaModel();
+        ninjaModel.setId(ninjaDTO.getId());
+        ninjaModel.setNome(ninjaDTO.getNome());
+        ninjaModel.setEmail(ninjaDTO.getEmail());
+        ninjaModel.setIdade(ninjaDTO.getIdade());
+        ninjaModel.setImgUrl(ninjaDTO.getImgUrl());
+        ninjaModel.setRank(ninjaDTO.getRank());
+        ninjaModel.setMissoes(ninjaDTO.getMissoes());
+
+        return ninjaModel;
+    }
+
+    public NinjaDTO map(NinjaModel ninjaModel){
+        NinjaDTO ninjaDTO = new NinjaDTO();
+        ninjaDTO.setNome(ninjaModel.getNome());
+        ninjaDTO.setId(ninjaModel.getId());
+        ninjaDTO.setEmail(ninjaModel.getEmail());
+        ninjaDTO.setIdade(ninjaModel.getIdade());
+        ninjaDTO.setImgUrl(ninjaModel.getImgUrl());
+        ninjaDTO.setRank(ninjaModel.getRank());
+        ninjaDTO.setMissoes(ninjaModel.getMissoes());
+
+        return ninjaDTO;
+    }
 }
